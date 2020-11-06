aa96256444704b1eab57cf89bf87469df12e85b4
lera lescuyer <lllcommunication@gmx.fr>
2020-11-06 19:13:17 +0100

    fin

--- dist/js/app.js
+++ dist/js/app.js
@@ -26,7 +26,7 @@ form.addEventListener('submit', function (e) {
 
   //
 
-  if (isNaN(value) || value < 0 || value > 9) {
+  if (isNaN(value) || value <= 0 || value > 9) {
     // result.innerHTML = `<p class="result">${text[0]}</p>`
     result.innerHTML = `<p class="result">${text[random]}</p>`
   }else{
